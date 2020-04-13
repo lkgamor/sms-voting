a9b454b72d922b6cbf7d09a65c9955977cb7c8ab
Louis <lkgamor@gmail.com>
2020-04-13 02:53:05 -0700

    Updated voting-client Dockerfile

---
+++ voting-client/src/main/resources/db/migration/V3__add_candidate_email.sql
@@ -0,0 +1,15 @@
+-- -----------------------------------------------------
+-- Table `votes`.`candidate`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `votes`.`candidate` (
+  `candidate_id` VARCHAR(36) NOT NULL,
+  `candidate_name` VARCHAR(50) NOT NULL,
+  `candidate_email` VARCHAR(60),
+  `candidate_image` LONGBLOB,
+  `total_vote_count` INT(5) NOT NULL,
+  PRIMARY KEY (`candidate_id`),
+  UNIQUE INDEX `candidate_id_UNIQUE` (`candidate_id` ASC),
+  UNIQUE INDEX `candidate_name_UNIQUE` (`candidate_name` ASC))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
